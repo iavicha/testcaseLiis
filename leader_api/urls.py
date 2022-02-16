323414e9a8d3cabbf87413afb5d17e14acba23a8
ilia <ivetchinin@gmail.com>
2022-02-16 20:47:11 +0300

    done 5

--- leader_api/urls.py
+++ leader_api/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import LeaderPublicViewSet, LeaderSubscriptionViewSet, UserViewSet, LeaderList, LeaderListSus, \
-    LeaderAuthorSubscriptionViewSet
+from .views import LeaderPublicViewSet, LeaderSubscriptionViewSet, UserViewSet, LeaderAuthorSubscriptionViewSet
 
 router = routers.DefaultRouter()
-router.register(r'Public', LeaderPublicViewSet, basename='public')
-router.register(r'Subscription', LeaderSubscriptionViewSet, basename='subscription')
+router.register(r'public', LeaderPublicViewSet, basename='public')
+router.register(r'subscription', LeaderSubscriptionViewSet, basename='subscription')
 router.register(r'user', UserViewSet, basename='User')
 router.register(r'author', LeaderAuthorSubscriptionViewSet, basename='Author')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('', LeaderList.as_view()),
-    # path('registred/', LeaderListSus.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
